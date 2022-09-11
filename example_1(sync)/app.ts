e3d60522701659f7b01aed028f9e3c809d349367
choisungwook <kgg1959@naver.com>
2022-09-11 19:12:45 +0900

    동기 API호출 예제 추가

---
+++ example_1(sync)/app.ts
@@ -0,0 +1,40 @@
+type TodoInfo = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+function sleep(sec: number): void {
+  let start = Date.now(), now = start;
+  while (now - start < sec * 1000) {
+    now = Date.now();
+  }
+}
+
+function printDone(): void {
+  const rootContainer: HTMLElement | null = document.getElementById('root');
+
+  if (!rootContainer)
+    throw('root conatiner가 있는지 확인하세요');
+
+  const randomValue = Math.floor(Math.random() * 1000);
+  rootContainer.innerHTML = `
+    <h1>Done-${randomValue}!</h1>
+  `
+}
+
+function getData(): TodoInfo {
+  const URL: string = 'https://jsonplaceholder.typicode.com/todos/1';
+  const ajax: XMLHttpRequest = new XMLHttpRequest();
+
+  ajax.open('GET', URL, false);
+  ajax.send()
+
+  sleep(2);
+  return JSON.parse(ajax.response);
+}
+
+const data: TodoInfo = getData()
+console.log(data);
+printDone()
